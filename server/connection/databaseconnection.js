d6f1e633e04ee1d3a9cda065ccba579ebf782942
SushmaGK2024 <sushmagkulkarni23@gmail.com>
2024-05-11 04:18:26 +0530

    Update databaseconnection.js

--- server/connection/databaseconnection.js
+++ server/connection/databaseconnection.js
@@ -1,9 +1,13 @@
 // backend/db/connection.js
 const mysql = require('mysql2');
 
-const urldb = 'mysql://${{process.env.MYSQLUSER}}:${{process.env.MYSQL_ROOT_PASSWORD}}@${{process.env.RAILWAY_TCP_PROXY_DOMAIN}}:${{process.env.RAILWAY_TCP_PROXY_PORT}}/${{process.env.MYSQL_DATABASE}}'
-
-const db = mysql.createConnection(urldb);
+const db = mysql.createConnection({
+  host: process.env.MYSQLHOST, // Use the MYSQLHOST environment variable for the host
+  user: process.env.MYSQLUSER, // Use the MYSQLUSER environment variable for the user
+  password: process.env.MYSQLPASSWORD, // Use the MYSQLPASSWORD environment variable for the password
+  database: process.env.MYSQLDATABASE, // Use the MYSQLDATABASE environment variable for the database name
+  port: process.env.MYSQLPORT // Use the MYSQLPORT environment variable for the port
+});
 
 // Connect to MySQL
 db.connect((err) => {

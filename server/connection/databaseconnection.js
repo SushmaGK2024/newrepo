019adc0746422a0362148aba3b28aaeb323f80d6
SushmaGK2024 <sushmagkulkarni23@gmail.com>
2024-05-11 01:11:45 +0530

    Update databaseconnection.js

--- server/connection/databaseconnection.js
+++ server/connection/databaseconnection.js
@@ -2,10 +2,11 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'placementexp',
+  host: process.env.MYSQLHOST, // Use the MYSQLHOST environment variable for the host
+  user: process.env.MYSQLUSER, // Use the MYSQLUSER environment variable for the user
+  password: process.env.MYSQLPASSWORD, // Use the MYSQLPASSWORD environment variable for the password
+  database: process.env.MYSQLDATABASE, // Use the MYSQLDATABASE environment variable for the database name
+  port: process.env.MYSQLPORT // Use the MYSQLPORT environment variable for the port
 });
 
 // Connect to MySQL

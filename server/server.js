a282507a7dd3d31e29d6e4f84ec0a7aaf2cab347
SushmaGK2024 <sushmagkulkarni23@gmail.com>
2024-05-11 03:05:22 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,21 +4,23 @@ const db = require('./connection/databaseconnection'); // Adjust the path as nee
 const  cors=require('cors');
 const app = express();
 // Add this line to parse JSON bodies
-const port = 3001;
 app.use(express.json());
+const port = 3001;
 
-
-const netlifyDomain = 'https://main--legendary-madeleine-b1810c.netlify.app'; // Update with your Netlify domain
+// Set up routes...
+// server/server.js
+// ...
+// Example in server.js
 app.use(cors({
-  origin: netlifyDomain,
+  origin: 'http://localhost:3000',
+  origin: process.env.MYSQLHOST,
   credentials: true
 }));
-app.options('*', cors());
-
-
-
-
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 const usersRoutes = require('./routes/users.js');
 const experiencesRoutes = require('./routes/experiences.js');
 const placementsRoutes= require( './routes/placements.js' );
@@ -30,7 +32,10 @@ app.use('/extract-questions', extractQuestions);
 app.get('/', (req, res) => {
     res.send('Hello, this is the root path!');
   });
-  
+
+
+
+
 
 
   

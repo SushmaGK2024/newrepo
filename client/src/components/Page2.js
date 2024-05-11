58bb0ad21bf4a77d9e2d888a8e7d02b276e6c089
SushmaGK2024 <sushmagkulkarni23@gmail.com>
2024-05-11 06:07:23 +0530

    jjj

--- client/src/components/Page2.js
+++ client/src/components/Page2.js
@@ -76,7 +76,7 @@ const Page2 = ({ currrollno, dispatch, navigate }) => {
       const body = JSON.stringify(newExperience);
 
       const res = await axios.post(
-        "http://localhost:3001/experiences/add-experience",
+        "https://majorsssssss.onrender.com/experiences/add-experience",
         body,
         config
       );
